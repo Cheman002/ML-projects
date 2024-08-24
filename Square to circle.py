f664464b4aafb446c30656e87d2fbc0aa1b4e43a
Cheman002 <Cheman002@gmail.com>
2024-08-24 23:31:19 +0530

    Add files via upload

---
+++ Square to circle.py
@@ -0,0 +1,12 @@
+import turtle
+t= turtle.Turtle()
+t.speed(.25)
+for i in range(1000):
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(157)
---
+++ square.py
@@ -0,0 +1,10 @@
+import turtle
+t= turtle.Turtle()
+t.speed(.25)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
